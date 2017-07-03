cf9d6a2ed74e675a8aaf3a239ea04e736fb9adf2
Jo Van Bulck <Jo.VanBulck@cs.kuleuven.be>
2017-07-03 15:59:53 +0200

    Removed old iMinds logo.

--- 2.0/footer.php
+++ 2.0/footer.php
@@ -3,9 +3,9 @@
 <div class="span3">
     <div class="side-logos" data-spy="affix">
         <a href="https://distrinet.cs.kuleuven.be/"><img class="logo" src="logo_distrinet.png" /></a>
-        <br />
-        <a href="http://www.iminds.be/"><img class="logo" src="logo_iminds.png" /></a>
-        <br />
+        <!--br /-->
+        <!-- a href="http://www.iminds.be/"><img class="logo" src="logo_iminds.png" /></a-->
+        <!-- br /-->
         <!--p class="logo">This work has been supported in part by the Intel Lab's University Research Office.</p-->
     </div>
 </div>
